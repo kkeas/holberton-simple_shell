4e965363bde54ffef08f6bc80ba2459ea124d0a0
kkeas <keaskatrina@gmail.com>
2022-12-09 21:31:08 -0800

    fixed the prompt

--- prompt.c
+++ prompt.c
@@ -1,16 +1,11 @@
-#include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
+y#include "shell.h"
+
+/**
+ *  prompt - what to print
+ *  Return: none
+ */
 
 void prompt(void)
 {
-	char *buffer = malloc(1024);
-	size_t len = 1024;
-
-	while (1)
-	{
-		printf("$ ");
-		getline(&buffer, &len, stdin);
-	}
-	return (0);
-}	
+	write(STDOUT_FILENO, "$ ", 2); /* print the dollar sign to std out*/
+}
