1098ac4886ef3b6fcc2d4abc5c833f38820c0a30
kkeas <keaskatrina@gmail.com>
2022-12-10 05:37:10 -0800

    creates child process

---
+++ child.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * child - creates child process
+ * @array: array of strings passed to new prog
+ * @av: args from main
+ * @cnt: number of times program is called
+ * Return: child exit status
+ */
+
+int child(char **array, char *av[], int cnt)
+{
+	int valstat, status;
+	struct stat buf;
+	pid_t ch;
+
+	if (array == NULL || array[0] == NULL)
+		return (EXIT_SUCCESS);
+
+	if (stat(array[0], &buf) == -1)
+	{
+		error_print(array, av, cnt, 126);
+		return (126);
+	}
+	ch = fork();
+
+	if (ch == 0)
+	{
+		valstat = execve(array[0], array, environ);
+	}
+	else
+	{
+		wait(&status);
+		valstat = WEXITSTATUS(status);
+	}
+	return (valstat);
+}
